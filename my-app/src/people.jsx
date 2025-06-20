b58d0836245e57dbaa11c7757801ff8827676f86
Bernard Krehula <krehula.bernard456@gmail.com>
2025-06-20 20:07:29 +0200

    Define style

--- my-app/src/people.jsx
+++ my-app/src/people.jsx
@@ -4,27 +4,31 @@ const people = [
         name: 'susan',
         lastName: 'smith',
         job: 'web developer',
-        info: "I'm baby meggings twee health goth +1. Bicycle rights tumeric chartreuse before they sold out chambray pop-up. Shaman humblebrag pickled coloring book salvia hoodie, cold-pressed four dollar toast everyday carry"
+        info: "I'm baby meggings twee health goth +1. Bicycle rights tumeric chartreuse before they sold out chambray pop-up. Shaman humblebrag pickled coloring book salvia hoodie, cold-pressed four dollar toast everyday carry",
+        image: 'https://course-api.com/images/people/person-1.jpeg'
     },
     {
         id: 2,
         name: 'anna',
         lastName: 'johnson',
         job: 'web designer',
-        info: 'Helvetica artisan kinfolk thundercats lumbersexual blue bottle. Disrupt glossier gastropub deep v vice franzen hell of brooklyn twee enamel pin fashion axe.photo booth jean shorts artisan narwhal.'
+        info: 'Helvetica artisan kinfolk thundercats lumbersexual blue bottle. Disrupt glossier gastropub deep v vice franzen hell of brooklyn twee enamel pin fashion axe.photo booth jean shorts artisan narwhal.',
+        image: 'https://course-api.com/images/people/person-2.jpeg'
     },
     {
         id: 3,
         name: 'peter',
         lastName: 'jones',
         job: 'intern',
-        info: 'Sriracha literally flexitarian irony, vape marfa unicorn. Glossier tattooed 8-bit, fixie waistcoat offal activated charcoal slow-carb marfa hell of pabst raclette post-ironic jianbing swag.'
+        info: 'Sriracha literally flexitarian irony, vape marfa unicorn. Glossier tattooed 8-bit, fixie waistcoat offal activated charcoal slow-carb marfa hell of pabst raclette post-ironic jianbing swag.',
+        image: 'https://course-api.com/images/people/person-3.jpeg'
     },
     {
         id: 4,
         name: 'bill',
         lastName: 'anderson',
         job: 'the boss',
-        info: 'Edison bulb put a bird on it humblebrag, marfa pok pok heirloom fashion axe cray stumptown venmo actually seitan. VHS farm-to-table schlitz, edison bulb pop-up 3 wolf moon tote bag street art shabby chic.'
+        info: 'Edison bulb put a bird on it humblebrag, marfa pok pok heirloom fashion axe cray stumptown venmo actually seitan. VHS farm-to-table schlitz, edison bulb pop-up 3 wolf moon tote bag street art shabby chic.',
+        image: 'https://course-api.com/images/people/person-4.jpeg'
     }
 ]
